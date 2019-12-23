3978d951a611b53d77b9fd023d0c3fbc210c9d2a
benassa-de-glassa <33694956+benassa-de-glassa@users.noreply.github.com>
2019-12-23 18:21:01 +0100

    added comments and removed print statements

--- api/routes.py
+++ api/routes.py
@@ -49,7 +49,6 @@ def country_option():
     group = request.args.get('groups')
     dataset = request.args.get('dataset')
 
-    # FILE_NAME = os.path.join(DATA_WORKING_DIRECTORY, group, dataset)
     meta = {}
     with h5py.File(DATA_WORKING_DIRECTORY, 'r') as f:
         attributes = f["{}/{}".format(group, dataset)].attrs
@@ -67,62 +66,55 @@ def data_set_table():
     return the files in the folder selected as 
     """
 
+    # get the dimension names
     meta = [*request.args][2:]
-    # print(meta)
+
     # get the file
     group = request.args.get('groups')
     dataset = request.args.get('dataset')
 
-    # FILE_NAME = os.path.join(DATA_WORKING_DIRECTORY, group, dataset)
-
-    # # get the selected rows
-    # COUNTRIES = request.values.getlist('selected_countries[]')
-    # PRODUCTS = request.values.getlist('selected_products[]')
-    # ELEMENTS = request.values.getlist('selected_elements[]')
-
-    # TODO:
-    # - find and identify the meta-tags with the corresponding codes
-    # - find the correct rows to add to 'data'-variable
+    # open the hdf5 file
     with h5py.File(DATA_WORKING_DIRECTORY, 'r') as f:
+        # shortcut for data
         data_table = f["{}/{}".format(group, dataset)]
         data_table_shape = data_table.shape
 
+        # shortcut for headers
         header = data_table.attrs['headers']
 
+        # array of row numbers which are available in the data frame
         available_lines = np.arange(data_table_shape[0])
 
+        # initialization of the selected set variables
         lines = set()
 
+        # iterate over all dimensions specified in the request
         for dimension in meta:
-            # get the name of the selected dimension tags from the request
-
+            # grab all values of the dimesion
             requested_values = request.values.getlist('{}'.format(dimension))[0].split(',')
-            print(requested_values)
             # table containing the conversion from name to 
             conversion_table = data_table.attrs['{}_attribution'.format(dimension)]
             conversion_dict = {item[1]: item[0] for item in conversion_table}
 
-            # get the column in which the codes are used
-            column_index = np.where(conversion_table[0][0] == header)[0]
-
             # convert the requsted values to the corresponding dimension item code
             requested_codes = [float(conversion_dict[value]) for value in requested_values]
 
-            # iterate through all lines
-            # print(np.sum( np.array([bool(x in requested_codes) for x in data_table[:, column_index]])))
+            # get the column in which the codes are used
+            column_index = np.where(conversion_table[0][0] == header)[0]
 
-
-            # print(data_table[:, column_index], requested_codes)
+            # find the lines which match the codes
             mask = np.in1d(data_table[:, column_index], requested_codes)
+            # update the lines set
             lines.update(set(mask * available_lines))
+            # the line 0 has to be checked additionally as every 'False' mask index adds the 0 index to the set
             if not mask[0]: lines.discard(0)
 
-
+        # sort the lines
         lines = sorted(list(lines))
-        # lines *= np.array([bool(x in requested_codes) for x in data_table[:, column_index]])
 
-        
+        # initialize the data list
         data = []
         for line in lines:
+            # add the dict formated line to data which will then be returned
             data.append({header[i]: np.nan_to_num(data_table[line, i]) for i in range(len(header)-len(meta))})
         return jsonify( data = data)
