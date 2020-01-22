4c4af817a6e3696c60e46bf85c25d1371bdd0733
benassa-de-glassa <33694956+benassa-de-glassa@users.noreply.github.com>
2020-01-22 14:04:31 +0100

    API now uses codes not labels to find

--- api/routes.py
+++ api/routes.py
@@ -12,7 +12,6 @@ from . import api
 
 ## DEFINITIONS
 CWD = os.getcwd()
-# DATA_WORKING_DIRECTORY = os.path.join(CWD, '/media/pi/UNTITLED/database.hdf5')
 DATA_WORKING_DIRECTORY = os.path.join(CWD, 'data/database.hdf5')
 
 
@@ -23,7 +22,7 @@ def folder_option():
     i.e. the list of folders which host the databases
     """
     with h5py.File(DATA_WORKING_DIRECTORY, 'r') as f:
-        groups = [{'value': index, 'label': value.replace(',', ''), 'type': 'groups'} \
+        groups = [{'value': index, 'label': value} \
             for index, value in enumerate(f.keys())]
         return jsonify(groups=groups)
 
@@ -36,13 +35,13 @@ def data_set_option():
     group = request.args.get('groups')
 
     with h5py.File(DATA_WORKING_DIRECTORY, 'r') as f:
-        datasets = [{'value': index, 'label': value.replace(',', ''), 'type': 'dataset'}
+        datasets = [{'value': index, 'label': value, 'type': 'dataset'}
                     for index, value in enumerate(f[group].keys())]
         return jsonify(dataset=datasets)
 
 
 @api.route('/api/meta', methods=['GET'])
-def country_option():
+def meta_option():
     """
     return jsonify response of metadata
     """
@@ -54,7 +53,7 @@ def country_option():
         attributes = f["{}/{}".format(group, dataset)].attrs
         # years = attributes ['years'].tolist()
         for item in attributes['dimensions']:
-            meta[item] = [{'value': _item[0], 'label': _item[1].replace(',', ''), 'type': item} \
+            meta[item] = [{'value': _item[1], 'label': _item[0]} \
                 for _item in attributes['{}'.format(item)][1:]]
 
         return jsonify(attributes=attributes['dimensions'].tolist(), meta=meta)
@@ -73,64 +72,57 @@ def data_set_table():
     group = request.args.get('groups')
     dataset = request.args.get('dataset')
 
+
     # open the hdf5 file
     with h5py.File(DATA_WORKING_DIRECTORY, 'r') as f:
-        # shortcut for data
+        # get the data table
         data_table = f["{}/{}".format(group, dataset)]
-        data_table_shape = data_table.shape
 
-        # shortcut for headers
+        # grab the header attribute
         header = data_table.attrs['headers']
+        available_rows = np.arange(data_table.shape[0], dtype = int)
 
-        # array of row numbers which are available in the data frame
-        available_lines = np.arange(data_table_shape[0])
+        zero_is_valid_row = True
 
-        # initialization of the selected set variables
-        lines = np.arange(data_table_shape[0])
-
-        zero_is_valid_line = True
-        # iterate over all dimensions specified in the request
-        inverted_conversion_dicts = []
         for dimension in meta:
-            # grab all values of the dimesion
-            requested_values = request.values.getlist('{}'.format(dimension))[0].split(',')
-            # table containing the conversion from name to 
-            conversion_table = data_table.attrs['{}_attribution'.format(dimension)]
-            conversion_dict = {item[1]: item[0] for item in conversion_table}
-            inverted_conversion_dicts.append({value: item for item, value in conversion_dict.items()})
+            requested_codes = request.values.getlist('{}'.format(dimension))[0].split(',')
+            requested_codes = np.asarray(requested_codes, dtype = int)
 
+            conversion_dict = {int(entry[1]): entry[0] for entry in data_table.attrs['{}'.format(dimension)][1:]}
+            # requested_codes = np.asarray(requested_codes, dtype = np.int8)
+            if dimension == 'years':
+                # the requested columns always contain the requested dimensions
+                columns = list(range(len(meta)-1))
+                # add columns for each requested year
+                for year in requested_codes:
+                    # convert each year to the corresponding label
+                    columns.append(np.where(conversion_dict[year] == header)[0][0])
+            else:
+                # get the column in which the codes are used
+                column_index = np.where(data_table.attrs['{}'.format(dimension)] [0][1] == header)[0]
 
-            # convert the requsted values to the corresponding dimension item code
-            requested_codes = [float(conversion_dict[value]) for value in requested_values]
-            print(requested_values)
-            print(type(requested_codes[0]))
+                # find the lines which match the codes
+                mask = np.in1d(data_table[:, column_index], requested_codes)
+                available_rows *= mask
 
-            # get the column in which the codes are used
-            column_index = np.where(conversion_table[0][0] == header)[0]
-
-            # find the lines which match the codes
-            mask = np.in1d(data_table[:, column_index], requested_codes)
-            print(dimension, np.sum(mask))
-            # update the lines set
-            lines *= mask
-            print(set(available_lines * mask))
-
-            # the line 0 has to be checked additionally as every 'False' mask index adds the 0 index to the set
-            zero_is_valid_line *= mask[0] == True
-
+                # the line 0 has to be checked additionally as every 'False' mask index adds the 0 index to the set
+                zero_is_valid_row *= mask[0] == True
+        
         # sort the lines
-        lines = sorted(list(set(lines)))
-        if not zero_is_valid_line: lines.remove(0)
+        rows = sorted(list(set(available_rows)))
+        if not zero_is_valid_row: rows.remove(0)
 
-        # initialize the data list
+
         data = []
-        for line in lines:
+        for row in rows:
             # add the dict formated line to data which will then be returned
-            data.append({header[i]: np.nan_to_num(data_table[line, i]) for i in range(len(header)-len(meta))})
-            data[-1].update({
-                header[i]: inverted_conversion_dicts[j][data_table[line, i]] for j, i in enumerate(range(len(header)-len(meta), len(header)))
-                })
-        header_dict = [{'name': item, 'type': 'string'} for item in header]
-        columns = [{'text': item, 'datafield': item, 'width': '100px'} for item in header]
+            data += [{int(row): data_table[row, columns].tolist()}]
+            # data.append({header[i]: np.nan_to_num(data_table[line, i]) for i in range(len(header)-len(meta))})
+            # data[-1].update({
+            #     header[i]: inverted_conversion_dicts[j][data_table[row, i]] for j, i in enumerate(range(len(header)-len(meta), len(header)))
+            #     })
 
-        return jsonify(header=header_dict, columns=columns, data = data)
+
+        columns = list(map(str, columns))
+        # return jsonify(columns)
+        return jsonify(header=header.tolist(), columns=columns, data = data)
